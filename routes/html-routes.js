861247afe73477b352f3e613c2462b4d3881de2a
Sungpil An <ansungpil1@gmail.com>
2021-01-30 14:27:44 -0500

    finished html-api-routes.js, student-api-routes.js, and language-api.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,15 +3,19 @@ var path = require("path");
 module.exports = function(app) {
     // Index (Main Page) route loads main.handlebars
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public"));
+        res.sendFile(path.join(__dirname, "../public/"));
     });
     // Post route to load post.handlebars
     app.get("/post", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public"));
+        res.sendFile(path.join(__dirname, "../public/"));
     });
     // List route to load list.handlebars
     app.get("/list", function(req, res) {
-        res.sendFile(path.)
-    }
+        res.sendFile(path.join(__dirname, "../public/" ));
+    });
+    // Saved route to load saved.handlebars
+    app.get("/saved", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/"));
+    });
 
-}
+};
