56c8c613f07a27ab104938f49df2571aa3a3cb75
Brian Hestir <brhestir@gmail.com>
2021-01-31 16:34:28 -0500

    Refactoring: [schema.sql] - Only schema + names->2 match specification [seeds.sql] Just seed commands + data

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,29 @@
--- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS project2starter;
--- Creates the "todolist" database --
-CREATE DATABASE project2starter;
+DROP DATABASE IF EXISTS fresh-dev-db;
+
+CREATE DATABASE fresh-dev-db;
+
+USE fresh-dev-db;
+
+CREATE TABLE language (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+    language VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE student (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+    fullName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    school VARCHAR(100),
+    description TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE student_language (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	student_id INTEGER,
+	language_id INTEGER,
+    FOREIGN KEY (language_id) REFERENCES language (id),
+    FOREIGN KEY (student_id) REFERENCES student (id),
+    PRIMARY KEY(id)
+);
