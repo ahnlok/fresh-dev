253b841e621a6b26d1bb48973f33aeb0e02990be
Caleb Glorvigen <jcglorvi@gmail.com>
2021-02-02 15:06:06 -0500

    Polished up the code.

--- models/language.js
+++ models/language.js
@@ -1,14 +1,13 @@
 // Language.js
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Language = sequelize.define("Language", {
         language: DataTypes.STRING,
-    },
-    {       // This is the options object
+    }, { // This is the options object
         timestamps: false,
     });
-    
-    Language.associate = function(models) {
+
+    Language.associate = function (models) {
         Language.belongsToMany(models.Student, {
             through: "StudentLanguage",
             foreignKey: "language_id",
