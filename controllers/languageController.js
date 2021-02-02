8f869c48576530095db71f60de8ccb82e9376044
Sahil Patel <Patelsaho@gmail.com>
2021-02-02 11:49:31 -0500

    Formatted languageController.js

--- controllers/languageController.js
+++ controllers/languageController.js
@@ -6,19 +6,19 @@ const db = require("../models");
 const router = express.Router();
 
 /**
- * Route to render all students to a page. 
+ * Route to render all students to a page.
  */
 router.get("/", (req, res) => {
-    db.Language.findAll()
-      .then((allLanguages) => {
-        console.log(allLanguages);
-        res.render("index", { languages: allLanguages });
-      })
-      .catch((err) => {
-        console.log(err);
-        //TODO: render 404 page if we're unable to return students
-        res.status(500).end();
-      });
-  });
+  db.Language.findAll()
+    .then((allLanguages) => {
+      console.log(allLanguages);
+      res.render("index", { languages: allLanguages });
+    })
+    .catch((err) => {
+      console.log(err);
+      //TODO: render 404 page if we're unable to return students
+      res.status(500).end();
+    });
+});
 
-  module.exports = router;
+module.exports = router;
