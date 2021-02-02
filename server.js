81122238e447f4568c1624587f692a1ac49f9017
Caleb Glorvigen <jcglorvi@gmail.com>
2021-02-02 15:05:21 -0500

    Polished up the code.

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 // Requiring necessary npm packages
 const express = require("express");
-// Please do not delete this line ("session") -Sung
 const session = require("express-session");
+
 // Requiring passport as we've configured it
-// Please do not delete this line ("passport") -Sung
 const passport = require("./config/passport");
 const exphbs = require("express-handlebars");
 const handlebars = require("handlebars");
@@ -19,26 +18,13 @@ const LanguageController= require("./controllers/languageController");
 // Setting up port
 const PORT = process.env.PORT || 8080;
 
-// Creating express app and configuring middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Please Do NOT delete this line ("public") -Sung
 app.use(express.static(__dirname + "/public"));
-// Please DO NOT delete this line ("keyboard cat")-Sung
-// We need to use session to Keep track of our user's login status
-// Do NOT Change or Delete -Sung
+
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
-// DO NOT Change or Delete -Sung
-
-// Requiring our routes
-// Do NOT Change or Delete -Sung
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-// require("./routes/language-api-routes.js")(app);
-// require("./routes/student-api-routes.js")(app);
-// Do NOT Change or Delete -Sung
 
 app.engine(
   "handlebars",
@@ -48,10 +34,6 @@ app.engine(
   })
 );
 app.set("view engine", "handlebars");
-
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
 
 app.get("/api/config", (req, res) => {
   res.json({
@@ -63,25 +45,6 @@ app.get("/api/config", (req, res) => {
 app.use(StudentController);
 app.use(LanguageController);
 
-// post - Sung
-// app.post("/api/language", function (req, res) {
-//   console.log(req.body);
-//   db.Language.create({
-//     userId: req.body.userId,
-//     studentId: req.body.studentId,
-//   }).then((newEntry) => {
-//     res.json({
-//       success: true,
-//       message: "Successfully Added Your Post.",
-//       data: newEntry,
-//     });
-//   }).catch((err) => {
-//     res.status(500);
-//     res.json({
-//       success: false,
-//     });
-//   });
-// });
 // Syncing our database and logging a message to the user upon success
 db.sequelize
   .sync()
